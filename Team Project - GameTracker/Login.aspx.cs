b9e7519865df5b323d71a2c66c0008c5fe980d67
NickRowlandson <nicholasrowlandson@gmail.com>
2016-06-14 15:05:27 -0400

    Implemented basic login functionality. No authentication

--- Team Project - GameTracker/Login.aspx.cs
+++ Team Project - GameTracker/Login.aspx.cs
@@ -5,6 +5,10 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+// Using statements required for EF DB access
+using Team_Project___GameTracker.Models;
+using System.Web.ModelBinding;
+
 namespace Team_Project___GameTracker
 {
     public partial class Login : System.Web.UI.Page
@@ -16,7 +20,32 @@ namespace Team_Project___GameTracker
 
         protected void LoginButton_Click(object sender, EventArgs e)
         {
+            // Use Ef to connect to the server
+            using (DefaultConnection db = new DefaultConnection())
+            {
+               
+                // get the id from the URL
+                int UserID = 1;
 
+                // get the user from the EF database
+                var adminUser = (from user in db.AdminUsers
+                                where user.UserID == UserID
+                                select user).FirstOrDefault();
+
+                // 
+                var email = adminUser.Email;
+                var password = adminUser.Password;
+
+                if((email == EmailTextBox.Text) && (password == PasswordTextBox.Text ))
+                {
+                    // redirect back to the manage games page
+                    Response.Redirect("~/ManageGames.aspx");
+                }
+                else
+                {
+
+                }
+            }
         }
     }
 }
