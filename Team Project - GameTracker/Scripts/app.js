70f9c47f68508fce0b644d00bdce70680c8a98e0
NickRowlandson <nicholasrowlandson@gmail.com>
2016-06-15 16:13:50 -0400

    Added JQuery for delete confirmation.

--- Team Project - GameTracker/Scripts/app.js
+++ Team Project - GameTracker/Scripts/app.js
@@ -1,1 +1,8 @@
 ﻿/* Custom JS */
+
+$(document).ready(function () {
+    console.log("App started...");
+    $("a.delete").click(function () {
+        return confirm("Are you sure you want to delete this record?");
+    })
+})
